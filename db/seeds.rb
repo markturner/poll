070ceb84e61353ddc190b4beb033a75db209d128
Mark Turner <markjturner@gmail.com>
2013-02-09 11:59:58 +0000

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+# parties
+csv_text = File.read('db/seeds/parties.csv')
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  Party.find_or_create_by_abbreviation(row.to_hash.symbolize_keys)
+end
+puts "#{Party.count} parties seeded"
+
+# candidates and constituencies
+csv_text = File.read('db/seeds/candidates.csv')
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  row = row.to_hash.symbolize_keys
+  constituency = Constituency.find_or_create_by_name(name: row[:Seat])
+  candidate = Candidate.find_or_create_by_name( name: row[:Candidate],
+                                                incumbent: row[:Sitting].present? ? true : false,
+                                                constituency: constituency,
+                                                party: Party.find_by_abbreviation(row[:Party])
+                                              )
+end
+puts "#{Candidate.count} candidates seeded"
+puts "#{Constituency.count} constituencies seeded"
